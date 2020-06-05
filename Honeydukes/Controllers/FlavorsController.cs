7e10ecf432fc7340dbe521459c3c8460f674fa6d
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-05 10:32:39 -0700

    add flavor details view and controller

--- Honeydukes/Controllers/FlavorsController.cs
+++ Honeydukes/Controllers/FlavorsController.cs
@@ -38,5 +38,15 @@ namespace Honeydukes.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpGet]
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.Treats)
+        .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
   }
 }
