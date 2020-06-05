bb5632f1832e1c8913c9b1c8f8a4502e9859d42e
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-05 14:14:47 -0700

    add authentication functionality

---
+++ Honeydukes/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+using Honeydukes.Models;
+using Honeydukes.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Honeydukes.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly HoneydukesContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, HoneydukesContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    [HttpGet]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      var user = new ApplicationUser { Email = model.Email, UserName = model.UserName };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    [HttpGet]
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
+  }
+}
+
